eecb7659679a84e9a4f06f29f9df85b6a6bf48fb
josericardopenase <josericardopenase@gmail.com>
2025-04-30 11:38:09 +0100

    added sam segmentator

---
+++ segmentators/sam_2_segmentator.py
@@ -0,0 +1,28 @@
+from ultralytics import SAM
+from segmentators.segmentator import Segmentator, Result, BoundingBox, Segmentation
+from PIL import Image
+import numpy as np
+
+
+class SAM2Segmentator(Segmentator):
+    def __init__(self, model: str="sam2.1_b.pt"):
+        self.model = SAM(model)
+
+    def segment(self, image: Image.Image) -> Result:
+        # Convert the PIL image to a numpy array (which the model accepts)
+        image_np = np.array(image)
+
+        # Perform segmentation on the image
+        results = self.model(image_np)
+        bounding_boxes = []
+        segmentations = []
+
+        for result in results:
+            for box, segmentation in zip(result.boxes, result.masks):
+                x_center, y_center, width, height = box.xywh[0]
+                x_0, y_0 = int(x_center - width / 2), int(y_center - height / 2)
+                x_1, y_1 = int(x_0 + width), int(y_0 + height)
+                bounding_boxes.append(BoundingBox(x_0=x_0, y_0=y_0, x_1=x_1, y_1=y_1))
+                segmentations.extend(Segmentation(points=[(int(x), int(y)) for x, y in seg]) for seg in segmentation.xy)
+
+        return Result(bounding_boxes=bounding_boxes, segmentations=segmentations)
